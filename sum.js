2b6d1cb584a556630602a998aed39bb451f005ba
RueiYuHuang <b98170022@gmail.com>
2022-07-30 12:04:28 +0800

    add sum.js

---
+++ sum.js
@@ -0,0 +1,13 @@
+function sum(innValue){
+    let valueCount = 0;
+    for(let i = 1 ;i <= innValue; i++){
+        valueCount += i;
+        // console.log(valueCount)
+    }
+    return valueCount ;
+}
+console.log(sum(1)) 
+console.log(sum(2)) 
+console.log(sum(3)) 
+console.log(sum(4)) 
+console.log(sum(5))
