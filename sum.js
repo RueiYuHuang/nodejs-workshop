6ccaf4496802ed65947d4f7fcdd3e3484c343474
RueiYuHuang <b98170022@gmail.com>
2022-07-30 14:58:27 +0800

    update sum.js

--- sum.js
+++ sum.js
@@ -1,13 +1,25 @@
-function sum(innValue){
-    let valueCount = 0;
-    for(let i = 1 ;i <= innValue; i++){
-        valueCount += i;
+//A. for-loop
+function sum(innValue) {
+    let result = 0;
+    for (let i = 1 ;i <= innValue; i++) {
+        //這行執行次數會跟輸入成正比
+        //時間複雜度0(n)
+        result += i;
         // console.log(valueCount)
     }
-    return valueCount ;
+    return result ;
 }
 console.log(sum(1)) 
 console.log(sum(2)) 
 console.log(sum(3)) 
 console.log(sum(4)) 
 console.log(sum(5))
+
+//B. 公式解
+function sum2(n) {
+    //不管n多少 ，這行都只跑1次
+    //時間複雜度 0(1) -> 常數
+    return ((n + 1) * n ) / 2;
+}
+
+//資料結構 演算法 !?
